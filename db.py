abca90f35ac11a58735c4a9aa0836cefb45f6178
Jaxon Adams <jadamsresume1@gmail.com>
2023-04-23 10:32:43 -0600

    setting up db connection for prod

--- db.py
+++ db.py
@@ -1,6 +1,9 @@
 import pymongo
+import os
 
 def connect_to_db():
-    myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+    connection_string = os.environ.get('CONNECTION_STRING') or 'mongodb://localhost:27017/'
+
+    myclient = pymongo.MongoClient(connection_string)
     db = myclient['tinyurl']
     return db['tinyurl_urls']
